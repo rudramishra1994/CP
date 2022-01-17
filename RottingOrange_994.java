c129bbe8c54ba07967afc6ea6de5e65c5baca3ca
rudramishra1994 <mishra.r@northeastern.edu>
2022-01-17 16:54:48 -0500

    Intitlal commit

---
+++ RottingOrange_994.java
@@ -0,0 +1,17 @@
+package cp;
+public class RottingOrange_994 {
+    /* 
+    *classic bfs
+    */
+    public int orangesRotting(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int rotten = 0;
+        int fresh = 0 ;
+        for(int i = 0 ; i< row ;i++){
+            for (int j = 0 ;j< col; j++){
+                
+            }
+        }
+    }
+}
