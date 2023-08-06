1c4973a7396b21ef3ae4520108a46c652e7d8a60
Rudra Mohan Mishra <91039543+rudramishra1994@users.noreply.github.com>
2023-08-06 04:15:03 -0400

    Time: 1 ms (42.74%), Space: 39.4 MB (67.13%) - LeetHub

---
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.java
@@ -0,0 +1,24 @@
+/*
+f(n) = f(n-1)+f(n-2)+2f(n-3)+2f(n-4)+2f(n-5) + ... + 2f(0) ...i
+f(n-1) = f(n-2)+f(n-3)+2f(n-4)+2f(n-5)+...+2f(0) ...ii
+
+subtracting equation ii from i we get
+f(n) = 2f(n-1) + f(n-3) [recurrence relation]
+
+*/
+class Solution {
+    public int numTilings(int n) {
+
+        if(n <= 2) return n;
+        int MOD = 1000000007;
+        long [] dp = new long[n+1];
+    
+        dp[1] = 1; dp[2] = 2; dp[3] = 5;
+        for(int i = 4 ; i < n+1 ; i++){
+            dp[i] = (2 * dp[i-1] + dp[i-3])%MOD;
+        }
+
+        return (int)dp[n] % MOD;
+        
+    }
+}
