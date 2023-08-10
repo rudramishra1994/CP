42b25a01f23af0967888f448620f41938f003902
Rudra Mohan Mishra <91039543+rudramishra1994@users.noreply.github.com>
2023-08-10 11:43:31 -0400

    Time: 1 ms (98.52%), Space: 43.7 MB (90.9%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,11 +3,11 @@ class Solution {
         int pos = 0;int k = 0;
         for(int i = 1 ; i < nums.length ; i++){
             if(nums[i]!=nums[pos]){
-                if(i-pos>=2)
-                    nums[++pos] = nums[i];
-                else pos++;
-            }else if(nums[i]==nums[pos])k++;
+                pos++;
+                nums[pos] = nums[i];
+            }
         }
-        return nums.length-k;
+        return pos+1;
+        
     }
 }
