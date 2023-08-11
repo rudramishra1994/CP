32eb7189c4256b7276ed3c6efefdff6fa09fcaea
Rudra Mohan Mishra <91039543+rudramishra1994@users.noreply.github.com>
2023-08-10 22:23:47 -0400

    Time: 1 ms (88.79%), Space: 44.2 MB (74.18%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,19 +1,23 @@
 class Solution {
     int [][] dp;
     public int maxProfit(int[] prices) {
-        dp = new int[prices.length+1][2];
-        for(int [] arr : dp) Arrays.fill(arr,-1);
-        return maxProfit(prices,0,1);    
+       int profit = 0;
+       for(int i = 1 ; i < prices.length;i++){
+           if(prices[i] > prices[i-1]) profit += prices[i] - prices[i-1];
+       }
+       return profit;
     }
 
-    private int maxProfit(int []prices,int day,int buying){
-        if(day > prices.length-1) return 0;
-        if(dp[day][buying]!=-1) return dp[day][buying];
-        if(buying == 1){
-            return dp[day][buying] = Math.max(maxProfit(prices,day+1,1),maxProfit(prices,day+1,0)-prices[day]);
-        }else {
-            return dp[day][buying] = Math.max(maxProfit(prices,day+1,0),maxProfit(prices,day+1,1)+prices[day]);
-        }
-    }
+
+    //This is the generic approach for Best time to buy and sell stock
+    // private int maxProfit(int []prices,int day,int buying){
+    //     if(day > prices.length-1) return 0;
+    //     if(dp[day][buying]!=-1) return dp[day][buying];
+    //     if(buying == 1){
+    //         return dp[day][buying] = Math.max(maxProfit(prices,day+1,1),maxProfit(prices,day+1,0)-prices[day]);
+    //     }else {
+    //         return dp[day][buying] = Math.max(maxProfit(prices,day+1,0),maxProfit(prices,day+1,1)+prices[day]);
+    //     }
+    // }
 
 }
