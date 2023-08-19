fe03713c49262b256378b358f461704d0232e743
Rudra Mohan Mishra <91039543+rudramishra1994@users.noreply.github.com>
2023-08-19 19:18:26 -0400

    Time: 3 ms (6.01%), Space: 43.9 MB (7.01%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void gameOfLife(int[][] board) {
+        int [][] dirs = {{-1,0},{1,1},{0,1},{-1,1},{1,0},{1,-1},{0,-1},{-1,-1}};
+        int n = board.length;
+        int m = board[0].length;
+        for(int i = 0; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                int count = 0;
+                for(int [] dir : dirs){
+                        int nextX = i+dir[0];
+                        int nextY = j+dir[1];
+                        if(isValid(nextX,nextY,n,m) && isAlive(board[nextX][nextY])) count++;
+                }
+                if(board[i][j] == 1 && (count < 2 || count > 3)) board[i][j] = -999;
+                else if(board[i][j] == 1 && count <= 3) board[i][j] = 999;
+                else if(board[i][j] == 0 && count == 3) board[i][j] = 9999;
+            }
+        }
+        for(int i = 0; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(board[i][j] ==-999) board[i][j] = 0;
+                else if(board[i][j] ==999 || board[i][j] ==9999) board[i][j] = 1;
+            }
+        }
+    }
+
+    private boolean isValid(int nextX,int nextY,int n,int m){
+        if(0<=nextX && nextX < n && 0<=nextY && nextY < m) return true;
+        return false;
+    }
+
+    private boolean isAlive(int boardVal){
+        Set<Integer> alive = new HashSet<>(Arrays.asList(1,999,-999));
+        if(alive.contains(boardVal)) return true;
+        return false;
+    }
+}
