951347da9b5c112eaf7051965bb5e51dc46089e0
Rudra Mohan Mishra <91039543+rudramishra1994@users.noreply.github.com>
2023-09-20 17:46:59 -0400

    Time: 16 ms (61.9%), Space: 42.7 MB (93.68%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,25 +1,44 @@
 class Solution {
-    int MAX_COUNT = 999999;
-    int [] dp;
+    //int MAX_COUNT = 999999;
+    //int [] dp;
     public int coinChange(int[] coins, int amount) {
         
-        this.dp = new int [amount+1];
-        Arrays.fill(this.dp,-1);
-        this.dp[0] = 0;
-        int count = countCoin(coins,amount);
-        return count >= MAX_COUNT ? -1 :count;
+        // this.dp = new int [amount+1];
+        // Arrays.fill(this.dp,-1);
+        // this.dp[0] = 0;
+        // int count = countCoin(coins,amount);
+        // return count >= MAX_COUNT ? -1 :count;
 
-    }
-
-    public int countCoin(int [] coins,int amount){
-        if(amount == 0) return 0;
-        if(dp[amount]!=-1) return dp[amount];
-        int count = MAX_COUNT;
-        for(int coin : coins){
-            if(amount >= coin){
-                count = Math.min(count,countCoin(coins,amount-coin));
+        int [] dp = new int [amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0] = 0;
+        for(int i = 1; i <=amount ;i++){
+            for(int coin : coins){
+                if(coin <= i){
+                    dp[i] = Math.min(dp[i],1+dp[i-coin]);
+                }
             }
         }
-        return dp[amount] =  1+count;
+        return dp[amount] <= amount ? dp[amount]:-1;
+
     }
+
+
+
+
+
+
+
+
+    // public int countCoin(int [] coins,int amount){
+    //     if(amount == 0) return 0;
+    //     if(dp[amount]!=-1) return dp[amount];
+    //     int count = MAX_COUNT;
+    //     for(int coin : coins){
+    //         if(amount >= coin){
+    //             count = Math.min(count,countCoin(coins,amount-coin));
+    //         }
+    //     }
+    //     return dp[amount] =  1+count;
+    // }
 }
