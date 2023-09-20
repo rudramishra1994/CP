f594adac4b0f2e3d0cd9686ac5394b1712116b8a
Rudra Mohan Mishra <91039543+rudramishra1994@users.noreply.github.com>
2023-09-20 02:37:58 -0400

    Time: 0 ms (100%), Space: 39.2 MB (40.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,7 +1,6 @@
 class Solution {
     public int climbStairs(int n) {
         if( n <3) return n;
-        int [] dp = new int [n+1];
         int x = 1 ;
         int y  = 2 ;
         int z = 0;
@@ -9,7 +8,6 @@ class Solution {
             z = x + y;
             x = y;
             y = z;
-
         }
         return z;
     }
