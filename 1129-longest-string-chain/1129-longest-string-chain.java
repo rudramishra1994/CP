5163cf31deeda5156f1746b1192a00a1061a1169
Rudra Mohan Mishra <91039543+rudramishra1994@users.noreply.github.com>
2023-09-23 15:35:13 -0400

    Time: 38 ms (68.29%), Space: 44.2 MB (11.57%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.java
+++ 1129-longest-string-chain/1129-longest-string-chain.java
@@ -3,12 +3,15 @@ class Solution {
     public int longestStrChain(String[] words) {
         if(words.length < 2) return words.length;
         Set<String> wordSet = new HashSet<>(Arrays.asList(words));
-        int maxOverAll = 1;
-        for(String word : words){
-            //maxOverAll = Math.max(maxOverAll,solveBruteForce(word,wordSet));
-            maxOverAll = Math.max(maxOverAll,solveTopfDown(word,wordSet));
-        }
-        return maxOverAll;
+        // int maxOverAll = 1;
+        // for(String word : words){
+        //     //maxOverAll = Math.max(maxOverAll,solveBruteForce(word,wordSet));
+        //     maxOverAll = Math.max(maxOverAll,solveTopfDown(word,wordSet));
+        // }
+
+        // return maxOverAll;
+
+        return solveBottomUP(wordSet,words);
     }
 
     public int solveTopfDown (String word ,Set<String> wordSet){
@@ -32,5 +35,23 @@ class Solution {
             maxVal = Math.max(maxVal, 1 + solveBruteForce(substring,wordSet));
         }
         return maxVal;
+    }
+
+
+    public int solveBottomUP(Set<String> wordSet,String [] words){
+         Arrays.sort(words,(a,b)->a.length()-b.length());
+         int maxOverAll = 1;
+         for(String word : words){
+             int currLen = 1;
+             for(int i = 0 ; i < word.length();i++){
+                 StringBuilder br = new StringBuilder(word);
+                 br.deleteCharAt(i);
+                 currLen = Math.max(currLen,dp.getOrDefault(br.toString(),0)+1);
+             }
+             dp.put(word,currLen);
+             maxOverAll = Math.max(maxOverAll,currLen);
+         }
+         return maxOverAll;
+
     }
 }
