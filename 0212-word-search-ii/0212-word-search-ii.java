f905476f8abd9a91f3b5166fd0e0a9c057c9b196
Rudra Mohan Mishra <91039543+rudramishra1994@users.noreply.github.com>
2023-09-28 14:05:15 -0400

    Time: 49 ms (94.38%), Space: 45 MB (39.91%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -0,0 +1,91 @@
+class Solution {
+
+    TrieNode root;
+    
+    public List<String> findWords(char[][] board, String[] words) {
+        createTrie(words);
+        Set<String> ans = new HashSet<>();
+        for(int i = 0 ; i < board.length;i++)
+            for(int j = 0 ; j < board[0].length;j++)
+                backtrack(board,i,j,root,new StringBuilder (),ans);
+        return new ArrayList<>(ans);
+
+        
+    }
+
+    private void backtrack(char [][] board,int x,int y,TrieNode node,StringBuilder br,Set<String> ans){
+        if(x < 0 || y < 0 || x >=board.length || y >=board[0].length || board[x][y] =='*') return ;
+        char c = board[x][y];
+        if(node.children[c-'a']==null) return;
+        br.append(c);
+        if(node.children[c-'a'].isEndNode){
+            ans.add(br.toString());
+        }
+        int [][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        board[x][y] = '*';
+        for(int [] dir : dirs){
+            backtrack(board,x+dir[0],y+dir[1],node.children[c-'a'],br,ans);
+        }
+        board[x][y] = c;
+        br.deleteCharAt(br.length()-1);
+        if(node.children[c-'a'].isLeaf()) node.children[c-'a'] = null;
+    }
+
+    private void createTrie(String [] words){
+        root = new TrieNode();
+        for(String word : words){
+            addWord(word);
+        }
+    }
+
+
+    private class TrieNode{
+        boolean isEndNode;
+        TrieNode [] children;
+
+        TrieNode(){
+            isEndNode = false;
+            children = new TrieNode[26];
+        }
+        boolean isLeaf(){
+            for(int i = 0 ; i < 26;i++){
+                if(children[i]!=null) return false;
+            }
+            return true;
+        }
+    }
+    private void addWord(String word){
+        TrieNode tempNode = root;
+        for(char c : word.toCharArray()){
+            if(tempNode.children[c-'a'] == null){
+                tempNode.children[c-'a'] = new TrieNode();
+            }
+            tempNode = tempNode.children[c-'a'];
+        }
+        tempNode.isEndNode = true;
+    }
+
+    // private boolean isPresent(String word){
+    //     TrieNode tempNode = root;
+    //      for(char c : word.toCharArray()){
+    //         if(tempNode.children[c-'a'] == null){
+    //             return false;
+    //         }
+    //         tempNode = tempNode.children[c-'a'];
+    //     }
+    //     return tempNode.isEndNode ;
+    // }
+
+    // private boolean startsWith(String word){
+    //     TrieNode tempNode = root;
+    //      for(char c : word.toCharArray()){
+    //         if(tempNode.children[c-'a'] == null){
+    //             return false;
+    //         }
+    //         tempNode = tempNode.children[c-'a'];
+    //     }
+    //     return true ;
+    // }
+
+
+}
