099e37222d29ab42411eaf7af9fca8294d161142
Rudra Mohan Mishra <91039543+rudramishra1994@users.noreply.github.com>
2023-09-30 17:54:17 -0400

    Time: 257 ms (5.02%), Space: 44.1 MB (55.4%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if(t.length() > s.length()) return "";
+        Map<Character,Integer> s_freq = new HashMap<>();
+        Map<Character,Integer> t_freq = new HashMap<>();
+        String minStr = "";int minStrLen = s.length();
+
+        for(char c : t.toCharArray()){
+            t_freq.put(c,t_freq.getOrDefault(c,0)+1);
+        }
+
+        int left = 0, start = 0, right = s.length()-1;
+
+        while(left <=right){
+            char c = s.charAt(left);
+            if(t_freq.containsKey(c)) {
+                int count = s_freq.getOrDefault(c,0);
+                s_freq.put(c,count+1);
+            }
+            if(left-start+1 >=t.length()){ 
+                while(isWindowValid(s_freq,t_freq)){
+                    if(minStrLen >= left-start+1){
+                        minStrLen = left - start +1;
+                        minStr = s.substring(start,left+1);
+                        if(minStrLen == t.length()) return minStr;
+                    }
+                    char ch = s.charAt(start++);
+                    if(s_freq.containsKey(ch)){
+                        int count  = s_freq.get(ch);
+                        s_freq.put(ch,count-1);
+                        if(s_freq.get(ch)==0) s_freq.remove(ch);
+                    }
+                }
+                
+            }
+            left++;
+        }
+        return minStr;
+
+    }
+
+    private boolean isWindowValid(Map<Character,Integer> s_freq , Map<Character,Integer> t_freq){
+        
+        for(char c : t_freq.keySet()){
+            if( !(s_freq.containsKey(c) && t_freq.get(c) <=s_freq.get(c))) return false;
+        }
+        return true;
+    }
+} 
