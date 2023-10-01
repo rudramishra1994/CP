7aaf1859df089f5031aa9167276ee7d4d00fec05
Rudra Mohan Mishra <91039543+rudramishra1994@users.noreply.github.com>
2023-10-01 03:16:17 -0400

    Time: 2 ms (100%), Space: 44.1 MB (14.85%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,25 +1,25 @@
 class Solution {
     public int romanToInt(String s) {
        int left = 0, n = s.length()-1;
-       Map<Character,Integer> rD = new HashMap<>();
-       rD.put('I',1);
-       rD.put('V',5);
-       rD.put('X',10);
-       rD.put('L',50);
-       rD.put('C',100);
-       rD.put('D',500);
-       rD.put('M',1000);
+       int [] dec = new int [26];
+       dec['I'-'A'] = 1;
+       dec['V'-'A'] = 5;
+       dec['X'-'A'] = 10;
+       dec['L'-'A'] = 50;
+       dec['C'-'A'] = 100;
+       dec['D'-'A'] = 500;
+       dec['M'-'A'] = 1000;
        int res = 0;
        while(left <=n){
             char ch = s.charAt(left);
-            if(left == n) return res + rD.get(ch);
+            if(left == n) return res + dec[ch-'A'];
             char next = s.charAt(left+1);
-            if(rD.get(ch) < rD.get(next)) {
-                res+=rD.get(next) - rD.get(ch);
+            if(dec[ch-'A'] < dec[next-'A']) {
+                res+=dec[next-'A'] - dec[ch-'A'];
                 left = left+2;
                 
             }else {
-                res+=rD.get(ch);
+                res+=dec[ch-'A'];
                 left++;
             }    
        }
