590b7e001223ed44536ab5d2ebc11b0cfcebbc5b
Rudra Mohan Mishra <91039543+rudramishra1994@users.noreply.github.com>
2023-10-01 03:12:30 -0400

    Time: 5 ms (52.05%), Space: 43.4 MB (89.8%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -2,10 +2,6 @@ class Solution {
     public int romanToInt(String s) {
        int left = 0, n = s.length()-1;
        Map<Character,Integer> rD = new HashMap<>();
-       Map<Character,Set<Character>> nBor = new HashMap<>();
-       nBor.put('C',new HashSet<>(Arrays.asList('D','M')));
-       nBor.put('X',new HashSet<>(Arrays.asList('L','C')));
-       nBor.put('I',new HashSet<>(Arrays.asList('X','V')));
        rD.put('I',1);
        rD.put('V',5);
        rD.put('X',10);
