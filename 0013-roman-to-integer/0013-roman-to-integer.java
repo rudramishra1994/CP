8babf94a1ea4cbae728f9ee7019c0c17fd029887
Rudra Mohan Mishra <91039543+rudramishra1994@users.noreply.github.com>
2023-10-01 03:01:56 -0400

    Time: 8 ms (8.32%), Space: 43.8 MB (28.94%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int romanToInt(String s) {
+       int left = 0, n = s.length()-1;
+       Map<Character,Integer> rD = new HashMap<>();
+       Map<Character,Set<Character>> nBor = new HashMap<>();
+       nBor.put('C',new HashSet<>(Arrays.asList('D','M')));
+       nBor.put('X',new HashSet<>(Arrays.asList('L','C')));
+       nBor.put('I',new HashSet<>(Arrays.asList('X','V')));
+       rD.put('I',1);
+       rD.put('V',5);
+       rD.put('X',10);
+       rD.put('L',50);
+       rD.put('C',100);
+       rD.put('D',500);
+       rD.put('M',1000);
+       int res = 0;
+       while(left <=n){
+            char ch = s.charAt(left);
+            if(left == n) return res + rD.get(ch);
+            char next = s.charAt(left+1);
+            if(ch == 'M' || ch == 'D' || ch == 'V' || ch == 'L') {
+                res+=rD.get(ch);
+                left++;
+                continue;
+            }
+            if(nBor.get(ch).contains(next)) {
+                res+=rD.get(next) - rD.get(ch);
+                left = left+2;
+                continue;
+            }
+            res+=rD.get(ch);
+            left++;
+       }
+       return res;
+    }
+}
