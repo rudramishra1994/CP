f134428b4a61ebb9fd1427b63a598a4e051d10c8
Rudra Mohan Mishra <91039543+rudramishra1994@users.noreply.github.com>
2023-10-21 18:55:27 -0400

    Time: 7 ms (49.31%), Space: 44.3 MB (75.83%) - LeetHub

---
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canMakeSubsequence(String str1, String str2) {
+        int n = str1.length(), m = str2.length();
+        int i=0,j=0;
+        for(; i < n && j < m;i++){
+            if(str1.charAt(i) == str2.charAt(j) || str1.charAt(i)+1 == str2.charAt(j)||str1.charAt(i)-25 == str2.charAt(j)) j++;
+        }
+        if(j == m) return true;
+        return false;
+
+    }
+}
