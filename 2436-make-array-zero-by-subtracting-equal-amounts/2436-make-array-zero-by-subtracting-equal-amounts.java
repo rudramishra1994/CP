c47b0c2bfe9844c5488199e118f6c5d6d9a1aede
Rudra Mohan Mishra <91039543+rudramishra1994@users.noreply.github.com>
2023-10-22 00:09:43 -0400

    Time: 0 ms (100%), Space: 40.6 MB (9.74%) - LeetHub

--- 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,22 +1,11 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Set<Integer> set = new HashSet<>();
         for(int i : nums){
-            if(i > 0) pq.add(i);
+            if( i > 0)
+            set.add(i);
         }
-        int count = 0;
-        while(!pq.isEmpty()){
-            int smallest = pq.poll();
-            count++;
-            PriorityQueue<Integer> pq2 = new PriorityQueue<>();
-            while(!pq.isEmpty()){
-                int k = pq.poll();
-                k -=smallest;
-                if(k > 0) pq2.add(k);
-            }
-            pq = pq2;
-        }
-        return count;
+        return set.size();
 
     }
 }
